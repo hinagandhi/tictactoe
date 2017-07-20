b26685b8be84c4fa7e80200c19751fefb7da0a30
hinagandhi <gandhi.hi@husky.neu.edu>
2017-07-19 20:38:24 -0400

    changes modified for heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from 'path'; // path package
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../webpack.config.dev';
+import webpackConfig from './webpack.config.dev';
 
 let app = express(); // initialize our app as express for all the functions
 let port = process.env.port || 3000;
@@ -17,10 +17,18 @@ app.use(webpackMiddleware(compiler,{
 }));
 app.use(webpackHotMiddleware(compiler));
 
-app.get('/', (req,res) => { // request to catch all routes
-   res.sendFile(path.join(__dirname, './index.html')); //redirect to index page
+// app.get('/', (req,res) => { // request to catch all routes
+//    res.sendFile(path.join(__dirname, './index.html')); //redirect to index page
+// });
+app.use( function(req, res, next){
+    if(req.headers['x-forwarded-proto'] === 'https'){
+        res.redirect("http://" + req.hostname + req.url);
+    }else{
+        next();
+    }
 });
 
+app.use(express.static('./'));
 // running application on localhost:3000
 
 app.listen(port,function(){
